d1630761a6d7205c6aecdd09cc0d4c63f5aeef02
Cuban Gnocchi <cubangnocchi@proton.me>
2025-01-13 14:33:28 -0500

    Organizando MazeBuilding

--- Resourses/Logic/Maze/MazeGenerator.cs
+++ Resourses/Logic/Maze/MazeGenerator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Runtime.InteropServices;
 using Colorful;
 using Spectre.Console.Rendering;
@@ -7,8 +7,55 @@ using Resourses.Tools;
 namespace Resourses.Logic;
 public partial class Maze
 {
+    public enum Type
+    {
+        Tutorial,
+        FindPlayer,
+        TeamWork,
+        Standard
+    }
     //[i] - Here I will put all the methods nessesary for
     //      for building the maze.
+
+    //[i] - First stage of the building process
+    //      Here I create "logic" rooms, connections
+    //      starting points and int[,] Lee for 
+    //      placing MapObjects
+
+    //[i] - Second stage of the building
+    //      process. Here the room cels are 
+    //      added and the MapObjects
+    //-------------------------------------
+
+    private void Builder (Type type)
+    {
+        switch(type)
+        {
+            Type.Tutorial : BuildTutorial()
+            Type.FindPlayer : BuildFindPlayer()
+            Type.TeamWork : BuildTeamWork()
+            Type.Standart : Build()
+
+        }
+
+    }
+
+    private void BuildTutorial()
+    {
+        
+    }
+    private void BuildFindPlayer()
+    {
+
+    }
+    private void BuildTeamWork()
+    {
+
+    }
+    private void Builder()
+    {
+
+    }
 
     
 }
